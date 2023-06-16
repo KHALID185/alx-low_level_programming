aad6e3f22915bb10661ee8a618a425144482df46
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-16 08:50:20 -0700

    “variable_if_for_while”

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible different combinations of two digits
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int cnum1 = 0;
+
+	while (cnum1 <= 9)
+
+	{
+		int cnum2 = 0;
+		while (cnum2 <= 9)
+		{
+		if (cnum1 != cnum2 && cnum1 < cnum2)
+		{
+			putchar(cnum1 + 48);
+			putchar(cnum1 + 48);
+			if (cnum1 + cnum2 != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		cnum2++;
+		}
+		cnum1++;
+	}
+	putchar('\n');
+	return (0);
+
+}
