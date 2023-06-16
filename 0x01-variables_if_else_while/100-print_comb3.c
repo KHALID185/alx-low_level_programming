7bcfbbf5b550e5f26d5dd09ff2164e3e67bbe16e
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-16 10:12:12 -0700

    “variable_if_for_while”

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,25 @@
 */
 int main(void)
 {
-	int cnum1 = 0;
+	int cnum1 = 0, cnum2;
 
 	while (cnum1 <= 9)
 
 	{
-		int cnum2 = 0;
+		cnum2 = 0;
 		while (cnum2 <= 9)
 		{
-		if (cnum1 != cnum2 && cnum1 < cnum2)
-		{
-			putchar(cnum1 + 48);
-			putchar(cnum1 + 48);
-			if (cnum1 + cnum2 != 17)
+			if (cnum1 != cnum2 && cnum1 < cnum2)
 			{
+			putchar(cnum1 + 48);
+			putchar(cnum1 + 48);
+				if (cnum1 + cnum2 != 17)
+				{
 				putchar(',');
 				putchar(' ');
+				}
 			}
-		}
-		cnum2++;
+			cnum2++;
 		}
 		cnum1++;
 	}
