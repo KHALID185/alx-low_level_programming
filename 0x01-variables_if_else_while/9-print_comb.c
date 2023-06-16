f0fa2dacba5657ec0fe926422a237540671707ed
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-16 08:30:34 -0700

    “variable_if_for_while”

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+
+}
