6b37729a060dca84c95e0625f9ff24606677a69c
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-15 17:33:17 -0700

    “101_quote”

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ *
+ * Return: 1 (fail)
+*/
+
+int main(void)
+{	char qte[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1. qte. 59);
+	return (1);
+}
