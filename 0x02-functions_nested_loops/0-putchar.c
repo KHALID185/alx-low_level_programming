2ea083c35b0d36f775ad1c4b3bc42851a915ef3a
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-20 10:07:32 -0700

    “functions_nestedloops”

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char string[] = "_putchar";
+unsigned int i;
+	for (i = 0; i <= strlen(string); i++)
+		_putchar(string[i]);
+	_putchar('\n');
+
+	return (0);
+
+}
