5abd934bd8df199510d2c36263a49109794502b2
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-20 14:41:50 -0700

    “functions_nestedloops”

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - use _putchar to print a...z
+ *
+*/
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+		_putchar(j);
+	_putchar('\n');
+}
