c5f92fec5e3f955e408b901bdd56eb8a875f2c21
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-21 16:03:42 -0700

    “debugging”

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 success
+*/
+
+int main(void)
+{
+	int s, m;
+
+	for (m = 0; m <= 1024; m++)
+	{
+	if ((m % 3 == 0) || (m % 5 == 0))
+		s = s + m;
+
+	}
+	print("%d\n", s);
+
+	return (0);
+
+}
