83032e85b99850c55ff26c739a50bfe5a3ab37c4
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-20 20:11:09 -0700

    “functions_nestedloops”

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @m: input type int
+ *
+ * Return: 0 success
+*/
+
+int _abs(int m)
+{
+	if (m < 0)
+		n = (-1) * n;
+	return (n);
+
+}
