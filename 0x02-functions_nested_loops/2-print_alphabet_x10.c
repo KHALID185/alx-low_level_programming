8ca33d3fdd01902ba437784b222ce0e9f8826142
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-20 19:28:30 -0700

    “functions_nestedloops”

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - use _putchar to print a...z 10 time
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i > 10; i++)
+	{
+		int j;
+
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		 _putchar('\n');
+	}
+}
