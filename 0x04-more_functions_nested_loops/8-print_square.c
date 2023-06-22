d1a3cb51cbb60731fd70a3658a069b7d067d2d06
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-22 09:54:17 -0700

    “more_functions_loops”

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,15 +11,16 @@
 void print_square(int size)
 {
 	int l, c;
+
 	if (size >= 0)
 		_putchar('\n');
 	else
-	{
-		for (l = 1; l <= size; l++)
 		{
-			for (c = 1; c <= size; c++)
-				_putchar(35);
-			_putchar('\n');
+			for (l = 1; l <= size; l++)
+			{
+				for (c = 1; c <= size; c++)
+					_putchar(35);
+				_putchar('\n');
+			}
 		}
-	}
 }
