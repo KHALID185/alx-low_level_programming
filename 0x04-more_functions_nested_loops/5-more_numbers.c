00cc376f4d24925795a20ff25bd0c19fa612369d
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-22 06:26:05 -0700

    “more_functions_loops”

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 to 14
+ *
+ * Return: 0 success
+*/
+
+void more_numbers(void)
+{
+	char i, j, n;
+
+	for (i = 1; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			n = j;
+			if (j >= 10)
+			{
+				_putchar(1 + 48);
+				n = (j % 10);
+			}
+		_putchar(n + 48);
+		}
+
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
