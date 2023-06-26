54bce8609c77dd9afbcc3d90b71be5782e91340c
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-26 09:36:09 -0700

    “pointers_arrays_strings”

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string.
+ * @s: parametre string
+*/
+
+void rev_string(char *s)
+{
+	int j, i;
+	char ch;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		for (j = 0; j < i / 2; j++)
+		{
+			ch = s[j];
+			s[j] = s[l - 1 - j];
+			s[l - 1 - j] = ch;
+
+		}
+
+}
