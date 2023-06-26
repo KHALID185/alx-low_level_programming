2a9f1e348bf89bd4d79fface75818eeca2c4154b
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-06-26 07:14:10 -0700

    “pointers_arrays_strings”

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+include "main.h"
+
+/**
+ * reset_to_98 - akes a pointer to an int as paramete
+ * @n: int parametr*
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write character
+ *
+ * @s: the character
+ *
+ * Return: 1 (success)
+*/
+
+int _putchar(char s)
+{
+	return (write(1, &s, 1));
+}
