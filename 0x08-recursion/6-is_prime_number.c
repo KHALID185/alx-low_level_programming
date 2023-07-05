85a6f6ad2f1642e2b405364b083d05c388594976
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-07-05 09:51:25 -0700

    “0x08_recursion”

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,7 @@
 int is_it_primenum(int n, int i);
 int is_prime_number(int n)
 {
-	return(is_it_primenum(n, 2));
+	return (is_it_primenum(n, 2));
 }
 
 /**
@@ -21,10 +21,10 @@ int is_prime_number(int n)
 */
 int is_it_primenum(int n, int i)
 {
-if (n > 1 && i > n+1)
+if (n > 1 && i > n + 1)
 	return (1);
 else if (n % i == 0 || n < 2)
 	return (0);
 else
-	return(is_it_primenum(n, i + 1));
+	return (is_it_primenum(n, i + 1));
 }
