a7969be5492ae78f66041042e109c942fceeba37
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-07-10 10:38:56 -0700

    “malloc_free”

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,9 +10,11 @@
 int **alloc_grid(int width, int height)
 {
 int **ptr, i, j;
-ptr = malloc(sizeof(int) * height);
+ptr = malloc(sizeof(*ptr) * height);
 if (width <= 0 || height <= 0 || ptr == 0)
+{
 	return (NULL);
+}
 else
 {
 	for (i = 0; i < height; i++)
@@ -20,10 +22,10 @@ else
 		ptr[i] = malloc(sizeof(**ptr) * width);
 		if (ptr[i] == 0)
 		{
-		while (i--)
-			free(ptr[i]);
-		free(ptr);
-		return (NULL);
+			while (i--)
+				free(ptr[i]);
+			free(ptr);
+			return (NULL);
 		}
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
