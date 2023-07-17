a54d25327fe0a07324791cfee0449b067200f988
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-07-17 09:05:47 -0700

    “structures_typedef”

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of dog
+ * @name:name of dog typr pointer
+ * @age: age of dog type float
+ * @owner: name of owner type pointer
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
