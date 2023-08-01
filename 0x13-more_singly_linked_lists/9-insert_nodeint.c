ae4b8a568fddd04d36f8b059d401b780da0f73d8
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-08-01 05:16:38 -0700

    more_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,32 +10,31 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *nd, *n_n = malloc(sizeof(listint_t));
-unsigned int j;
+	listint_t *nd, *n_n = malloc(sizeof(listint_t));
+	unsigned int j;
 
-if (!head || !n_n)
-	return (NULL);
-n_n->n = n;
-n_n->next = NULL;
-
-if (!idx)
-{
-	n_n->next = *head;
-	*head = n_n;
-	return (n_n);
-}
-nd = *head;
-while (nd)
-{
-	if (j == idx - 1)
+	if (!head || !n_n)
+		return (NULL);
+	n_n->n = n;
+	n_n->next = NULL;
+	if (!idx)
 	{
-		n_n->next = nd->next;
-		nd->next = n_n;
+		n_n->next = *head;
+		*head = n_n;
 		return (n_n);
 	}
-	j++;
-	nd = nd->next;
-}
-free(n_n);
-return (NULL);
+	nd = *head;
+	while (nd)
+	{
+		if (j == idx - 1)
+		{
+			n_n->next = nd->next;
+			nd->next = n_n;
+			return (n_n);
+		}
+		j++;
+		nd = nd->next;
+	}
+	free(n_n);
+	return (NULL);
 }
