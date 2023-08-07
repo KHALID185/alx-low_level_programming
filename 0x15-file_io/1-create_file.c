aa90e72e2b4ce1564e3f5f9dffdc3020813122af
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-08-07 03:44:30 -0700

    file_I_O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,6 +36,5 @@ int create_file(const char *filename, char *text_content)
 	if (l)
 		byt = write(f, text_content, l);
 	close(f);
-	byt = l ? 1 : -1;
-	return (byt);
+	return (byt == l ? 1 : -1);
 }
