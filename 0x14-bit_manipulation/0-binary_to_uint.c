e1e44650790c04fa7316a8d2a439450a350e3e51
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-08-31 06:12:06 -0700

    bit maniputlation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,9 +10,9 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-	while (b != NULL)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 			return (0);
