7fc216a23fe101acbc5044064d2fe7ffc361de85
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-09-04 07:31:56 -0700

    file in\out

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * l_str - length of a string
- * @strg: a string
- * Return: integer length
-*/
-
-int l_str(char *strg)
-{
-int j = 0;
-
-if (strg == NULL)
-	return (-1);
-
-for (j = 0; strg[j];)
-	j++;
-return (j);
-}
-
-/**
  * create_file - a function that creates a file.
  * @filename: pointer to the name of that file
  * @text_content: the content to put inside the filename
@@ -27,19 +9,22 @@ return (j);
 
 int create_file(const char *filename, char *text_content)
 {
-int  byt_print = 0, l_s;
-int f_d;
+int f_d, byt_w, l = 0;
 
-if (filename == NULL)
+if (!filename)
+return (-1);
+if (text_content)
+{
+	while (*text_content++)
+		l++;
+
+}
+
+f_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+byt_w = write(f_d, text_content, l);
+if (f_d == -1 || byt_w == -1)
 	return (-1);
-f_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (f_d == -1)
-	return (-1);
-l_s = l_str(text_content);
-if (l_s != -1)
-	byt_print = write(f_d, text_content, l_s);
-if (byt_print != l_s)
-	return (-1);
+
 close(f_d);
 
 return (1);
