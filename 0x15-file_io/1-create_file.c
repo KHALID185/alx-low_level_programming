9f3eb569bcb063b77ff37ec3138cf6ba9c58e136
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-09-04 07:24:02 -0700

    file in\out

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,9 +8,12 @@
 
 int l_str(char *strg)
 {
-int j;
+int j = 0;
 
-for (j = 0; strg[j] != '\0';)
+if (strg == NULL)
+	return (-1);
+
+for (j = 0; strg[j];)
 	j++;
 return (j);
 }
@@ -24,7 +27,7 @@ return (j);
 
 int create_file(const char *filename, char *text_content)
 {
-int  byt_print = 0, l_s = l_str(text_content);
+int  byt_print = 0, l_s;
 int f_d;
 
 if (filename == NULL)
@@ -32,9 +35,10 @@ if (filename == NULL)
 f_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (f_d == -1)
 	return (-1);
-if (l_s != 0)
+l_s = l_str(text_content);
+if (l_s != -1)
 	byt_print = write(f_d, text_content, l_s);
-if (byt_print == -1)
+if (byt_print != l_s)
 	return (-1);
 close(f_d);
 
