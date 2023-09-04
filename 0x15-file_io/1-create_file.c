561c058c3d029d107f3c89718cf6bd30d5eae46b
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-09-04 07:52:50 -0700

    file in\out

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * l_str - length of string
- * @str: a string
- * Return: integer
-*/
-
-int l_str(char *str)
-{
-	int j = 0;
-
-	for (j = 0; str[j];)
-		j++;
-	return (j);
-}
-
-/**
  * create_file - a function that creates a file.
  * @filename: pointer to the name of that file
  * @text_content: the content to put inside the filename
@@ -24,12 +9,16 @@ int l_str(char *str)
 
 int create_file(const char *filename, char *text_content)
 {
-int f_d, byt_w, l = l_str(text_content);
+int f_d, byt_w, l = 0;
 
 if (!filename)
 	return (-1);
-if (l == 0)
-	return (-1);
+if (text_content != NULL)
+{
+	while (*text_content++)
+		l++;
+
+}
 f_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 byt_w = write(f_d, text_content, l);
 
