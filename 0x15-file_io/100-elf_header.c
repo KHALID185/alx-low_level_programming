3284036f501bf7621a924e2dcb48a8904d809007
TAOUTI KHALID <khalid.1998.taouti@gmail.com>
2023-09-05 06:35:59 -0700

    ffff

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,22 +1,6 @@
-#include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-
-void ch_e(unsigned char *e_ident);
-void p_m(unsigned char *e_ident);
-void p_c(unsigned char *e_ident);
-void p_d(unsigned char *e_ident);
-void p_v(unsigned char *e_ident);
-void p_a(unsigned char *e_ident);
-void p_o(unsigned char *e_ident);
-void p_typ(unsigned int e_type, unsigned char *e_ident);
-void p_e(unsigned long int e_entry, unsigned char *e_ident);
-void c_e(int elf);
+#include "shell.h"
+#include "100-elf_header_1.c"
+#include "100-elf_header_2.c"
 
 
 /**
--- 0x15-file_io/shell.h
+++ 0x15-file_io/shell.h
@@ -9,14 +9,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-
-#define ER_NoELF "Error: Not an ELF file\n"
-#define LE "2's complement, little endian\n"
-#define BE "2's complement, big endian\n"
-#define No "Error: `%s`: No such file\n"
-#define ER_R "Error: Can't read file %s\n"
-#define ER_C "Error: Can't close fd %d\n"
-
 void ch_e(unsigned char *e_ident);
 void p_m(unsigned char *e_ident);
 void p_c(unsigned char *e_ident);
@@ -24,8 +16,8 @@ void p_d(unsigned char *e_ident);
 void p_v(unsigned char *e_ident);
 void p_a(unsigned char *e_ident);
 void p_o(unsigned char *e_ident);
-void p_t(unsigned int e_type, unsigned char *e_ident);
+void p_typ(unsigned int e_type, unsigned char *e_ident);
 void p_e(unsigned long int e_entry, unsigned char *e_ident);
-void c_e(int el);
+void c_e(int elf);
 
 #endif
