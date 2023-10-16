fab59f1e0e2d3e9bd9b0074ea25f23c2128c33f5
KHALID185 <khalid.1998.taouti@gmail.com>
2023-10-16 09:47:35 -0700

    dynamic lib

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,5 +24,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 
 #endif
